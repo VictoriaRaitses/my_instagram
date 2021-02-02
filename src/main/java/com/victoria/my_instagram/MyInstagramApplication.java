f46f64c828bbb88f71eb6b7847067284fe7942ba
VictoriaRaitses <victoria.raitses@gmail.com>
2021-02-02 14:18:57 +0100

    Initial Commit

---
+++ src/main/java/com/victoria/my_instagram/MyInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.victoria.my_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyInstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victoria/my_instagram/MyInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoria.my_instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
